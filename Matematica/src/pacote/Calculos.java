5d08fd0614576c8c16835d88f3cc3e42513334a9
profgeraldo <geraldo.cantelli@jala.university>
2025-05-08 16:48:16 -0300

    commit

---
+++ Matematica/src/pacote/Calculos.java
@@ -0,0 +1,32 @@
+package pacote;
+
+public class Calculos {
+
+	public int maior(int a, int b) {
+		int resp = 0;
+		if (a > b) {
+			resp = a;
+		}
+		if (a < b) {
+			resp = b;
+		}
+		return resp;
+	}
+	
+	
+	
+	
+	
+	
+	public int somar(int a, int b) {
+		int resp = 0;
+		// implementar aqui
+		return resp;
+	}
+
+	public double resolve1grau(double a, double b) {
+		double resp = 0;
+		// implementar aqui
+		return resp;
+	}
+}
---
+++ Matematica/src/pacote/Principal.java
@@ -0,0 +1,40 @@
+package pacote;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		// entrada de dados
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Digite um número:");
+		int num1 = ler.nextInt();
+		System.out.println("Digite outro número:");
+		int num2 = ler.nextInt();
+		
+		// fazer os cálculos
+		String resultado = "";
+		Calculos calc = new Calculos();
+		int maior = calc.maior(num1, num2);
+		if (maior == 0) {
+			resultado = "são iguais";
+		}
+		else {
+			resultado = "o maior é: " + maior;
+		}
+		
+		
+		// exibição do resultado
+		System.out.println(resultado);
+		
+		}
+	}
+		
+		
+		
+		/*
+		int soma = calc.somar(num1,num2);
+		System.out.println("Soma: "+soma);
+		*/
+	
